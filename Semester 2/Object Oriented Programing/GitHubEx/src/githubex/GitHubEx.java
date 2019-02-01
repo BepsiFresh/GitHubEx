835ed39469f573969caa7b0d4794ef646aa05573
djt22@students.ptcollege.edu <>
2019-02-01 09:49:44 -0500

    First Version

---
+++ Semester 2/Object Oriented Programing/GitHubEx/src/githubex/GitHubEx.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package githubex;
+
+/**
+ *
+ * @author Dorian
+ */
+public class GitHubEx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Trinkle();
+        
+    }
+    
+    public static void Trinkle(){
+    
+        System.out.println("Dorian Trinkle");
+    
+    }
+    
+}
